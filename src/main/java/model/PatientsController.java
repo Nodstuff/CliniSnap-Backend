1ffa77dfcb12f2539bc0dae35f27a4f32ba166e9
nodstuff <tom.meaney@mycit.ie>
2016-02-24 12:47:41 +0000

    Add Patient List Method

--- src/main/java/model/PatientsController.java
+++ src/main/java/model/PatientsController.java
@@ -4,17 +4,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by tmeaney on 11/02/16.
  */
 @RestController
 public class PatientsController {
 
-        @RequestMapping("/patient")
-        public Patient patient(@RequestParam(value="name", defaultValue="World") String name) {
-            return new Patient("999999000000","Tom","Meaney",30,"29 Friars Walk","Ballyphehane","Cork City");
-        }
+    @RequestMapping("/patient")
+    public Patient patient(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Patient("99999","Tom","Meaney",30,"29 Friars Walk","Ballyphehane","Cork City");
+    }
 
+    @RequestMapping("/patients")
+    public List<Patient> patients() {
+
+        List<Patient> pList = new ArrayList<>();
+        pList.add(new Patient("99999","Tom","Meaney",30,"29 Friars Walk","Ballyphehane","Cork City"));
+        pList.add(new Patient("12345","Jack","Hanley",22,"Uam-Var","Bishopstown","Cork"));
+        pList.add(new Patient("67890","James","Walsh",23,"Passage","PigTown","Cork"));
+        pList.add(new Patient("54321","Martina","O' Driscoll",23,"Barrack street","Cork","Cork"));
+        pList.add(new Patient("98765","Sean","Breen",25,"29 house road","Ballincollig","Cork"));
+
+        return pList;
+    }
 
 
 }
