2ea30871c678d3b7e1780baac91805e51df76d82
nodstuff <tom.meaney@mycit.ie>
2016-02-24 14:20:07 +0000

    add no arg constructor for user

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -2,12 +2,14 @@ package model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * Created by tmeaney on 24/02/16.
  */
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class User {
     private String username;
     private String password;
