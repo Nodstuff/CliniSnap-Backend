aeed319cefec5cb1d8b86da3b8cca7779d301f5f
nodstuff <tom.meaney@mycit.ie>
2016-02-25 11:08:32 +0000

    add list of patients to patient retrieve method to simulate a database

--- src/main/java/model/PatientsController.java
+++ src/main/java/model/PatientsController.java
@@ -16,11 +16,20 @@ public class PatientsController {
     @RequestMapping("/patient")
     public Patient patient(@RequestParam(value = "mrn", defaultValue = "") String mrn) {
 
-        if(mrn.equalsIgnoreCase("99999")){
-            return new Patient("99999","Tom","Meaney",30,"29 Friars Walk","Ballyphehane","Cork City");
-        }
-        return new Patient("1234","","",0,"","","");
+        List<Patient> pList = new ArrayList<>();
+        pList.add(new Patient("99999","Tom","Meaney",30,"29 Friars Walk","Ballyphehane","Cork City"));
+        pList.add(new Patient("88888","Alice","Muller",22,"29 Friars Walk","Ballyphehane","Cork City"));
+        pList.add(new Patient("12345","Jack","Hanley",22,"Uam-Var","Bishopstown","Cork"));
+        pList.add(new Patient("67890","James","Walsh",23,"Passage","PigTown","Cork"));
+        pList.add(new Patient("54321","Martina","O' Driscoll",23,"Barrack street","Cork","Cork"));
+        pList.add(new Patient("98765","Sean","Breen",25,"29 house road","Ballincollig","Cork"));
 
+        for(Patient p: pList){
+            if (p.getMrn().equals(mrn)) {
+                return p;
+            }
+        }
+        return new Patient("00000","Test","McTest",0,"TestRoad","TestTown","Cork");
     }
 
     @RequestMapping("/patients")
