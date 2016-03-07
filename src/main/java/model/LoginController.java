8eb8f53346570b312765033adf94dccffa7db95f
nodstuff <tom.meaney@mycit.ie>
2016-03-07 15:20:57 +0000

    Fix login method

--- src/main/java/model/LoginController.java
+++ src/main/java/model/LoginController.java
@@ -1,13 +1,11 @@
 package model;
 
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by tmeaney on 11/02/16.
  */
-@EnableOAuth2Sso
 @RestController
 public class LoginController {
 
