b20717d478b786aa7eb8c3ab603fc0f62f7131d2
nodstuff <tom.meaney@mycit.ie>
2016-03-10 16:02:32 +0000

    Add preliminary image stuff

---
+++ src/main/java/model/ImageController.java
@@ -0,0 +1,26 @@
+package model;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by tmeaney on 10/03/16.
+ */
+@RestController
+public class ImageController {
+
+    private String encoded = "";
+
+    @RequestMapping("/image")
+    public void saveImage(@RequestParam(value = "encodedImage", defaultValue = "") String encodedImage){
+        encoded = encodedImage;
+    }
+
+    @RequestMapping("/view-image")
+    public String viewImage(){
+        return "data:image/webp;base64,"+encoded;
+    }
+
+
+}
