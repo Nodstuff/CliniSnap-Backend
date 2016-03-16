8cd6e5845269d8b5ef527e0a4ad9fe787a5585c7
nodstuff <tom.meaney@mycit.ie>
2016-03-16 14:19:31 +0000

    Add write to database for token

--- src/main/java/model/LoginController.java
+++ src/main/java/model/LoginController.java
@@ -1,8 +1,13 @@
 package model;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.UUID;
 
 /**
@@ -11,11 +16,38 @@ import java.util.UUID;
 @RestController
 public class LoginController {
 
+    @Autowired
+    private DataSource datasource;
+
     @RequestMapping("/login")
     public UUID login() {
         UUID idOne = UUID.randomUUID();
-
+        connect(idOne);
         return idOne;
+    }
+
+    public void connect(UUID uuid){
+        String sql = "INSERT INTO \"user\" " +
+                "(auth_token) VALUES (?) WHERE username = ";
+        Connection conn = null;
+
+        try {
+            conn = datasource.getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, uuid.toString());
+            ps.executeUpdate();
+            ps.close();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+
+        } finally {
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) {}
+            }
+        }
     }
 
 }
