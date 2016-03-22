4efa50c5ee6a56b64dcb38ba84b5153062a21bc4
nodstuff <tom.meaney@mycit.ie>
2016-03-22 12:30:10 +0000

    Add patient db retrieval

--- src/main/java/model/PatientsController.java
+++ src/main/java/model/PatientsController.java
@@ -6,10 +6,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Timestamp;
+import java.sql.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -29,23 +26,25 @@ public class PatientsController {
     @RequestMapping("/patient")
     public Patient patient(@RequestParam(value = "mrn", defaultValue = "") String mrn) {
 
-        List<Patient> pList = new ArrayList<>();
-        pList.add(new Patient("99999","Tom","Meaney","20/07/1985","29 Friars Walk","Ballyphehane","Cork City","0858210277"));
-        pList.add(new Patient("88888","Alice","Muller","25/03/1994","29 Friars Walk","Ballyphehane","Cork City","0831412345"));
-        pList.add(new Patient("12345","Jack","Hanley","10/10/1993","Uam-Var","Bishopstown","Cork","0214543456"));
-        pList.add(new Patient("67890","James","Walsh","12/06/1991","Passage","PigTown","Cork","0891645342"));
-        pList.add(new Patient("54321","Martina","O' Driscoll","09/02/1992","Barrack street","Cork","Cork","0867546352"));
-        pList.add(new Patient("98765","Sean","Breen","19/12/1990","29 house road","Ballincollig","Cork","0648574635"));
-        pList.add(new Patient("240910","Oscar","Quill Walsh","07/11/1973","29 house road","Castle View","Cork","0648574635"));
+//        List<Patient> pList = new ArrayList<>();
+//        pList.add(new Patient("99999","Tom","Meaney","20/07/1985","29 Friars Walk","Ballyphehane","Cork City","0858210277"));
+//        pList.add(new Patient("88888","Alice","Muller","25/03/1994","29 Friars Walk","Ballyphehane","Cork City","0831412345"));
+//        pList.add(new Patient("12345","Jack","Hanley","10/10/1993","Uam-Var","Bishopstown","Cork","0214543456"));
+//        pList.add(new Patient("67890","James","Walsh","12/06/1991","Passage","PigTown","Cork","0891645342"));
+//        pList.add(new Patient("54321","Martina","O' Driscoll","09/02/1992","Barrack street","Cork","Cork","0867546352"));
+//        pList.add(new Patient("98765","Sean","Breen","19/12/1990","29 house road","Ballincollig","Cork","0648574635"));
+//        pList.add(new Patient("240910","Oscar","Quill Walsh","07/11/1973","29 house road","Castle View","Cork","0648574635"));
+//
+//
+//        for(Patient p: pList){
+//
+//            if (p.getMrn().equals(mrn)) {
+//                return p;
+//            }
+//        }
 
 
-        for(Patient p: pList){
-
-            if (p.getMrn().equals(mrn)) {
-                return p;
-            }
-        }
-        return new Patient("00000","Test","McTest","01/01/2000","TestRoad","TestTown","Cork","010987654");
+        return retrieve(mrn);
     }
 
     @RequestMapping("/patients")
@@ -63,7 +62,7 @@ public class PatientsController {
         return pList;
     }
 
-    public void connect(Patient patient){
+    public void insert(Patient patient){
 
         Date parsedDate = null;
 
@@ -112,5 +111,39 @@ public class PatientsController {
         }
     }
 
+    public Patient retrieve(String mrn){
+
+        Patient p = new Patient();
+
+        String sql = "SELECT * FROM \"patient\"" +
+                "WHERE mrn = ?";
+
+        Connection conn = null;
+
+        try {
+            conn = datasource.getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, mrn);
+            ResultSet rs = ps.executeQuery();
+
+            p.setMrn(rs.getString("mrn"));
+            p.setFname(rs.getString("fname"));
+            p.setSname(rs.getString("sname"));
+            p.setDob(rs.getString("date_of_birth"));
+            ps.close();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+
+        } finally {
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) {}
+            }
+        }
+
+        return p;
+    }
 
 }
