f21b9c9ae14bee8c53925b902172b56d3721f312
nodstuff <tom.meaney@mycit.ie>
2016-03-22 13:52:29 +0000

    Add project DAO

--- src/main/java/model/DAO.java
+++ src/main/java/model/DAO.java
@@ -4,6 +4,10 @@ package model;
  * Created by tmeaney on 22/03/16.
  */
 
+
+/**
+ * Created by nodstuff on 13/10/15.
+ */
 public interface DAO {
 
     int save(Object o);
--- src/main/java/model/MainConfig.java
+++ src/main/java/model/MainConfig.java
@@ -1,67 +1,28 @@
-package model;
-
-import org.apache.commons.dbcp2.BasicDataSource;
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.orm.hibernate4.HibernateTransactionManager;
-import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
-
-import javax.sql.DataSource;
-import java.net.URISyntaxException;
-import java.util.Properties;
-
-/**
- * Created by tmeaney on 16/03/16.
- */
-@Configuration
-public class MainConfig {
-
-
-
-    @Bean
-    public LocalSessionFactoryBean sessionFactory() throws URISyntaxException {
-        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-        sessionFactory.setDataSource(datasource());
-        sessionFactory.setPackagesToScan(new String[] { "model" });
-        sessionFactory.setHibernateProperties(hibernateProperties());
-
-        return sessionFactory;
-    }
-
-    @Bean
-    public DataSource datasource() throws URISyntaxException {
-        String dbUrl = System.getenv("JDBC_DATABASE_URL");
-        String username = System.getenv("JDBC_DATABASE_USERNAME");
-        String password = System.getenv("JDBC_DATABASE_PASSWORD");
-
-        BasicDataSource basicDataSource = new BasicDataSource();
-        basicDataSource.setUrl(dbUrl);
-        basicDataSource.setUsername(username);
-        basicDataSource.setPassword(password);
-
-        return basicDataSource;
-    }
-
-
-    @Bean
-    @Autowired
-    public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
-        HibernateTransactionManager txManager = new HibernateTransactionManager();
-        txManager.setSessionFactory(sessionFactory);
-
-        return txManager;
-    }
-
-    Properties hibernateProperties() {
-        return new Properties() {
-            {
-                setProperty("hibernate.hbm2ddl.auto", "create");
-                setProperty("hibernate.dialect", "org.hibernate.dialect.PostgresPlusDialect");
-                setProperty("hibernate.globally_quoted_identifiers", "true");
-                setProperty("hibernate.current_session_context_class","thread");
-            }
-        };
-    }
-}
+//package model;
+//
+//import org.apache.commons.dbcp2.BasicDataSource;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import java.net.URISyntaxException;
+//
+///**
+// * Created by tmeaney on 16/03/16.
+// */
+//@Configuration
+//public class MainConfig {
+//
+//    @Bean
+//    public BasicDataSource datasource() throws URISyntaxException {
+//        String dbUrl = System.getenv("JDBC_DATABASE_URL");
+//        String username = System.getenv("JDBC_DATABASE_USERNAME");
+//        String password = System.getenv("JDBC_DATABASE_PASSWORD");
+//
+//        BasicDataSource basicDataSource = new BasicDataSource();
+//        basicDataSource.setUrl(dbUrl);
+//        basicDataSource.setUsername(username);
+//        basicDataSource.setPassword(password);
+//
+//        return basicDataSource;
+//    }
+//}
