92331be9010f9844d4e265430234806c9a07d6ce
nodstuff <tom.meaney@mycit.ie>
2016-03-22 13:37:42 +0000

    Add project DAO

--- src/main/java/model/MainConfig.java
+++ src/main/java/model/MainConfig.java
@@ -1,19 +1,42 @@
 package model;
 
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.orm.hibernate4.HibernateTransactionManager;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.sql.DataSource;
 import java.net.URISyntaxException;
+import java.util.Properties;
 
 /**
  * Created by tmeaney on 16/03/16.
  */
+@EnableTransactionManagement
+@ComponentScan({ "model" })
 @Configuration
 public class MainConfig {
 
+
+
     @Bean
-    public BasicDataSource datasource() throws URISyntaxException {
+    public LocalSessionFactoryBean sessionFactory() throws URISyntaxException {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(datasource());
+        sessionFactory.setPackagesToScan(new String[] { "org.baeldung.spring.persistence.model" });
+        sessionFactory.setHibernateProperties(hibernateProperties());
+
+        return sessionFactory;
+    }
+
+    @Bean
+    public DataSource datasource() throws URISyntaxException {
         String dbUrl = System.getenv("JDBC_DATABASE_URL");
         String username = System.getenv("JDBC_DATABASE_USERNAME");
         String password = System.getenv("JDBC_DATABASE_PASSWORD");
@@ -24,5 +47,31 @@ public class MainConfig {
         basicDataSource.setPassword(password);
 
         return basicDataSource;
+    }
+
+
+    @Bean
+    @Autowired
+    public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+        HibernateTransactionManager txManager = new HibernateTransactionManager();
+        txManager.setSessionFactory(sessionFactory);
+
+        return txManager;
+    }
+
+    @Bean
+    public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
+        return new PersistenceExceptionTranslationPostProcessor();
+    }
+
+    Properties hibernateProperties() {
+        return new Properties() {
+            {
+                setProperty("hibernate.hbm2ddl.auto", "create");
+                setProperty("hibernate.dialect", "org.hibernate.dialect.PostgresPlusDialect");
+                setProperty("hibernate.globally_quoted_identifiers", "true");
+                setProperty("hibernate.current_session_context_class","thread");
+            }
+        };
     }
 }
