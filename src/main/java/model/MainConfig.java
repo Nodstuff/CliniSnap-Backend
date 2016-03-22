c32bb9f3efd4ce81f85c666159b8b1553e5d0469
nodstuff <tom.meaney@mycit.ie>
2016-03-22 13:41:24 +0000

    Add project DAO

--- src/main/java/model/MainConfig.java
+++ src/main/java/model/MainConfig.java
@@ -5,7 +5,6 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.orm.hibernate4.HibernateTransactionManager;
 import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
 
@@ -53,11 +52,6 @@ public class MainConfig {
         txManager.setSessionFactory(sessionFactory);
 
         return txManager;
-    }
-
-    @Bean
-    public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
-        return new PersistenceExceptionTranslationPostProcessor();
     }
 
     Properties hibernateProperties() {
