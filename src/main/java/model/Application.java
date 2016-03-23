7e573d19310a8ca28f2fa374e8be0a4ce2a2ce9e
nodstuff <tom.meaney@mycit.ie>
2016-03-23 13:23:52 +0000

    add logging and refactor project structure

--- src/main/java/model/Application.java
+++ src/main/java/model/Application.java
@@ -2,11 +2,13 @@ package model;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Created by tmeaney on 11/02/16.
  */
 @SpringBootApplication
+@ComponentScan
 public class Application {
 
     public static void main(String[] args) {
