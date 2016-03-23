109a50da476c0271b8344ddc1d69169a5a22b56c
nodstuff <tom.meaney@mycit.ie>
2016-03-23 18:43:12 +0000

    add update auth_token and push_token with UserDAO

--- src/main/java/xyz/nodstuff/model/User.java
+++ src/main/java/xyz/nodstuff/model/User.java
@@ -18,7 +18,7 @@ import java.sql.Date;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String username;
     private String useremail;
