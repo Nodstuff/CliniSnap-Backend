83dc94e2d0ee2c10b3d1e206b69e10869d788907
nodstuff <tom.meaney@mycit.ie>
2016-03-23 13:50:14 +0000

    add logging and refactor project structure

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -6,10 +6,11 @@ import org.springframework.context.annotation.ComponentScan;
  * Created by tmeaney on 11/02/16.
  */
 @SpringBootApplication
-@ComponentScan({"xyz.nodstuff.*"})
+@ComponentScan({"xyz.nodstuff.config","xyz.nodstuff.model","xyz.nodstuff.interfaces","xyz.nodstuff.gcm","xyz.nodstuff.crypto","xyz.nodstuff.controller"})
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 }
+
