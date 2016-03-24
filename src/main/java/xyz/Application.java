56d6a988165e0d2e71a3490cf30427dfbc1a4375
nodstuff <tom.meaney@mycit.ie>
2016-03-24 20:10:53 +0000

    add webapp

--- src/main/java/xyz/Application.java
+++ src/main/java/xyz/Application.java
@@ -3,11 +3,13 @@ package xyz;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
  * Created by tmeaney on 11/02/16.
  */
 @SpringBootApplication
+@EnableWebMvc
 @ComponentScan({"xyz.nodstuff"})
 public class Application {
 
