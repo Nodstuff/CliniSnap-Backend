7a447dfef1878cd7561567b71cac4ba33c55371b
nodstuff <tom.meaney@mycit.ie>
2016-03-24 20:08:01 +0000

    add webapp

--- src/main/java/xyz/nodstuff/controller/ImageController.java
+++ src/main/java/xyz/nodstuff/controller/ImageController.java
@@ -2,6 +2,7 @@ package xyz.nodstuff.controller;
 
 import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -49,5 +50,12 @@ public class ImageController {
     public ModelAndView viewImage() throws IOException {
         //return "data:image/webp;base64,"+encoded;
         return new ModelAndView("index").addObject("name", "Yashwant");
+    }
+
+
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
     }
 }
