f58752fc97f71b651bc3bf5b99540b39c91964a4
nodstuff <tom.meaney@mycit.ie>
2016-03-24 20:23:22 +0000

    add webapp

--- src/main/java/xyz/nodstuff/controller/ImageDisplayCounter.java
+++ src/main/java/xyz/nodstuff/controller/ImageDisplayCounter.java
@@ -1,9 +1,11 @@
 package xyz.nodstuff.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import xyz.nodstuff.interfaces.ImageDAO;
 
 /**
  * Created by tmeaney on 24/03/16.
@@ -11,9 +13,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class ImageDisplayCounter {
 
+    @Autowired
+    private ImageDAO imageDAO;
+
     @RequestMapping("/greeting")
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+
+        String image = "data:image/jpeg;base64," + imageDAO.findById(1).getImage();
+        
         model.addAttribute("name", name);
+        model.addAttribute("image", image);
         return "greeting";
     }
 
