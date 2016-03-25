a84d5fe2bce667649e46b4ea7c71013c9ed2e77d
nodstuff <tom.meaney@mycit.ie>
2016-03-25 11:54:46 +0000

    Add notification data to gcm payload

--- src/main/java/xyz/nodstuff/gcm/GcmSender.java
+++ src/main/java/xyz/nodstuff/gcm/GcmSender.java
@@ -43,7 +43,7 @@ public class GcmSender {
 
             jNotification.put("body","Great match!");
             jNotification.put("title","Portugal Vs. Denmark");
-            jNotification.put("icon","bons_m_transparent");
+            jNotification.put("icon","largebonslogo");
 
 
             jData.put("message", message);
